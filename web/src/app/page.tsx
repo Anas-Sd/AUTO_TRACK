5ea0b0a292a7b421cdeb85b053692085780e98d8
Anas-Sd <anasannu44455@gmail.com>
2026-09-14 18:54:13 +0530

    fix: hide manual log widget on desktop website, ensure category color picker is removed

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -57,8 +57,8 @@ function MainDashboard() {
 
       {/* Main Content Area */}
       <main className="flex-1 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-4 relative">
-        {/* Top 1/3rd Manual Log Widget Overlayed on Tabs */}
-        <div className="sticky top-2 z-30 mb-5 shadow-2xl">
+        {/* Top 1/3rd Manual Log Widget - Only available in Mobile / Android Application */}
+        <div className="md:hidden sticky top-2 z-30 mb-5 shadow-2xl">
           <ManualLogWidget />
         </div>
 
